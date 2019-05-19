267e40ff0150bf90880200f02622615092607dc7
rickhelmus <r.helmus@uva.nl>
2019-05-19 12:08:04 +0200

    updates for R3.6 (trial 1)

--- make_repos.R
+++ make_repos.R
@@ -1,12 +1,14 @@
 library("miniCRAN")
 
 # get regular and BioC repositories, omegahat for RDCOMClient
-repos <- c(BiocManager::repositories(), "http://www.omegahat.net/R")
+#repos <- c(BiocManager::repositories(), "http://www.omegahat.net/R")
+repos <- c(BiocManager::repositories()) # omegahat RDCOMClient doesn't work yet for R3.6, get it from GH
 
 pdb <- pkgAvail(repos = repos, type = "win.binary")
 pdb <- addPackageListingGithub(pdb = pdb, "rickhelmus/patRoon")
 pdb <- addPackageListingGithub(pdb = pdb, "rickhelmus/patRoonData")
 pdb <- addPackageListingGithub(pdb = pdb, "cbroeckl/RAMClustR")
+pdb <- addPackageListingGithub(pdb = pdb, "omegahat/RDCOMClient")
 # pdb <- miniCRAN:::addPackageListing(pdb, miniCRAN:::readDescription("~/Rproj/patRoon/DESCRIPTION"))
 
 pkgList <- pkgDep(c("patRoon", "patRoonData", "RAMClustR", "installr", "BiocManager", "rJava", "remotes", "pkgbuild", "RDCOMClient"),
@@ -21,27 +23,36 @@ makeGHPackage <- function(repos, pkgDir)
                     args = c("--no-test-load", ""))
 }
 
+onAppV <- !is.na(Sys.getenv("APPVEYOR", unset = NA))
+
 pkgDir <- tempfile("ghpkgs")
-#makeGHPackage("rickhelmus/patRoon", pkgDir)
+
+if (!onAppV)
+    makeGHPackage("rickhelmus/patRoon", pkgDir)
+    
 makeGHPackage("rickhelmus/patRoonData", pkgDir)
 makeGHPackage("cbroeckl/RAMClustR", pkgDir)
+makeGHPackage("omegahat/RDCOMClient", pkgDir)
 
-if (TRUE)
+RVers <- paste(R.Version()$major, floor(as.numeric(R.Version()$minor)), sep = ".")
+packagesFile <- paste0("bin/windows/contrib/", RVers, "/PACKAGES")
+
+if (file.exists(packagesFile))
 {
     regex <- "^Package: (.*)$" # inspired by https://github.com/andrie/miniCRAN/issues/79
-    packagesFile <- readLines("bin/windows/contrib/3.5/PACKAGES")
-    packages <- packagesFile[grepl(regex, packagesFile)]
+    pLines <- readLines(packagesFile)
+    packages <- pLines[grepl(regex, pLines)]
     packages <- gsub(regex, "\\1", packages)
     
     removedPackages <- packages[!packages %in% pkgList]
     newPackages <- pkgList[!pkgList %in% packages]
     
     # will be re-added
-    removedPackages <- union(removedPackages, c("patRoon", "patRoonData", "RAMClustR"))
+    removedPackages <- union(removedPackages, c("patRoon", "patRoonData", "RAMClustR", "RDCOMClient"))
     
-    if (length(removedPackages) > 0)
+    if (file.exists(packagesFile) && length(removedPackages) > 0)
     {
-        file.remove(list.files("bin/windows/contrib/3.5/", full.names = TRUE,
+        file.remove(list.files(paste0("bin/windows/contrib/", RVers), full.names = TRUE,
                                pattern = paste0(sprintf("^%s_.+\\.zip", removedPackages), collapse = "|")))
         updateRepoIndex(".", "win.binary")
     }
@@ -51,22 +62,26 @@ if (TRUE)
     
     updatePackages(".", repos = repos, ask = FALSE, type = "win.binary")
     
-    # should be downloaded as artifact from AppVeyor
-    addLocalPackage("patRoon", "C:/Projects", ".", "win.binary", build = FALSE, deps = TRUE)
     
-    addLocalPackage("patRoonData", pkgDir, ".", "win.binary", build = FALSE, deps = TRUE)
-    addLocalPackage("RAMClustR", pkgDir, ".", "win.binary", build = FALSE, deps = TRUE)
     
 } else
 {
-    unlink("bin", recursive = TRUE)
+    unlink(file.path("bin/windows/contrib", RVers), recursive = TRUE)
     makeRepo(pkgList, path = ".", repos = repos, type = c("win.binary"))    
-    
-    addLocalPackage("patRoon|patRoonData|RAMClustR", pkgDir, ".", "win.binary", build = FALSE, deps = TRUE)
-    addPackage(c("installr", "BiocManager", "rJava", "remotes", "pkgbuild"), ".", type = "win.binary") # needed for install script    
-    addPackage("RDCOMClient", ".", repos = c("http://www.omegahat.net/R", repos), type = "win.binary")
+    # addPackage(c("installr", "BiocManager", "rJava", "remotes", "pkgbuild"), ".", type = "win.binary") # needed for install script    
+    # addPackage("RDCOMClient", ".", repos = c("http://www.omegahat.net/R", repos), type = "win.binary")
 }
 
+if (onAppV)
+{
+    # should be downloaded as artifact from AppVeyor
+    addLocalPackage("patRoon", "C:/Projects", ".", "win.binary", build = FALSE, deps = TRUE)
+} else
+    addLocalPackage("patRoon", pkgDir, ".", "win.binary", build = FALSE, deps = TRUE)
+
+addLocalPackage("patRoonData", pkgDir, ".", "win.binary", build = FALSE, deps = TRUE)
+addLocalPackage("RAMClustR", pkgDir, ".", "win.binary", build = FALSE, deps = TRUE)
+addLocalPackage("RDCOMClient", pkgDir, ".", "win.binary", build = FALSE, deps = TRUE)
 
 
 # updatePackages(".", repos = repos, type = "win.binary", ask = FALSE)
